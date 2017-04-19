188a2da190027cfb906426f94dc55edfe33e0ef1
diql <diql@hotmail.com>
2017-04-19 11:13:43 +0800

    获取相关app信息;

--- app/src/main/java/org/diql/app/monitor/MainActivity.java
+++ app/src/main/java/org/diql/app/monitor/MainActivity.java
@@ -2,6 +2,7 @@ package org.diql.app.monitor;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -71,10 +72,36 @@ public class MainActivity extends AppCompatActivity {
             }
         };
         mLlApp.setAdapter(mAdapter);
+
+        printAppInfo();
     }
 
     private void initView() {
         mTvMsg = (TextView) findViewById(R.id.tv_msg);
         mLlApp = (ListView) findViewById(R.id.ll_app);
+    }
+
+    private void printAppInfo() {
+        StringBuilder strBuilder = new StringBuilder();
+        StringBuilder appBuilder = new StringBuilder();
+        StringBuilder itemBuilder = new StringBuilder();
+        String[] apps = getResources().getStringArray(R.array.Competitor_List);
+        String[] packages = getResources().getStringArray(R.array.app_packages);
+        for (String app : apps) {
+//            In :
+            for (AppBean appBean : mList) {
+                if (appBean != null && TextUtils.equals(app, appBean.getAppName())) {
+                    appBuilder.append("appName:").append(appBean.getAppName()).append(",")
+                            .append("appPackage:").append(appBean.getAppPackageName()).append("\n");
+                    strBuilder.append("\"").append(appBean.getAppPackageName()).append("\"").append(",");
+                    itemBuilder.append("\n<item>").append(appBean.getAppPackageName()).append("</item>");
+                    break;
+                }
+            }
+        }
+        System.out.println("appBuilder:" + appBuilder.toString());
+        System.out.println("strBuilder:" + strBuilder.toString());
+        Log.e("lalala","strBuilder:" + itemBuilder.toString());
+        System.out.println();
     }
 }
